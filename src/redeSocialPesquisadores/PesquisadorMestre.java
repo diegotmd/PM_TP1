bde0e10b333efbac25f37794da579e1b99aa788f
diegotmd <diegotmd@gmail.com>
2015-04-10 14:09:36 -0300

    Atualização Fonte

--- src/redeSocialPesquisadores/PesquisadorMestre.java
+++ src/redeSocialPesquisadores/PesquisadorMestre.java
@@ -2,21 +2,22 @@ package redeSocialPesquisadores;
 
 public class PesquisadorMestre extends Pesquisador {
 
-	private Integer qteAlunosGraduacaoOrientados;
-	
-	public Integer getQteAlunosGraduacaoOrientados() {
-		return qteAlunosGraduacaoOrientados;
-	}
+    private Integer qteAlunosGraduacaoOrientados;
 
-	public void setQteAlunosGraduacaoOrientados(
-			Integer qteAlunosGraduacaoOrientados) {
-		this.qteAlunosGraduacaoOrientados = qteAlunosGraduacaoOrientados;
-	}
-	
-	@Override
-	public Double calcularPopularidade() {
-		// TODO Auto-generated method stub
-		return null;
-	}	
+    public Integer getQteAlunosGraduacaoOrientados() {
+            return qteAlunosGraduacaoOrientados;
+    }
+
+
+    public PesquisadorMestre(Long id, Integer qteHorasIC, Integer qteHorasED, Integer qteAlunosGraduacao){
+        super(id, qteHorasIC, qteHorasED);
+        this.qteAlunosGraduacaoOrientados = qteAlunosGraduacao;
+    }
+
+    @Override
+    public Double calcularPopularidade() {
+            // TODO Auto-generated method stub
+            return null;
+    }	
 
 }
