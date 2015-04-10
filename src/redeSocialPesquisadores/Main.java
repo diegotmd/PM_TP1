8437a6cbbb9d4bca0b4b8ba1808d3e9958187f38
diegotmd <diegotmd@gmail.com>
2015-04-10 14:07:39 -0300

    Atualização Fonte

--- src/redeSocialPesquisadores/Main.java
+++ src/redeSocialPesquisadores/Main.java
@@ -1,10 +1,39 @@
 package redeSocialPesquisadores;
 
+import java.util.LinkedList;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        
+        LinkedList<Artigo> artigosRedeSocial;
+        LinkedList<Veiculo> veiculosRedeSocial;
+        LinkedList<Pesquisador> pesquisadoresRedeSocial;
+        
+        // lê pesquisadores do arquivo
+        pesquisadoresRedeSocial = Pesquisador.carregarPesquisadores("pesquisadores.txt");
+        
+        // lê veículos do arquivo
+        veiculosRedeSocial = Veiculo.carregarVeiculos("veiculos.txt");
+        
+        // lê artigos do arquivo
+        artigosRedeSocial = Artigo.carregarArtigos("artigos_veiculos.txt", veiculosRedeSocial);
+        
+        // lê publicações de artigos pelos pesquisadores
+        Pesquisador.carregarPublicacoes("grafo_artigos_pesquisadores.txt", pesquisadoresRedeSocial, artigosRedeSocial);
+        
+        // lê citações entre os artigos
+        Artigo.carregarCitacoes("grafo_citacoes.txt", artigosRedeSocial);
+        
+        // escreve popularidade dos pesquisadores
+        Pesquisador.escreverPopularidades("popularidade_pesquisador.txt", pesquisadoresRedeSocial);
+        
+        // escreve fator de impacto dos veículos
+        Veiculo.escreverFatorImpactoVeiculos("fatorImpacto_veiculo.txt", veiculosRedeSocial);
+        
+        // escreve qualidade dos artigos
+        Artigo.escreverQualidadeArtigos("pontuacao_artigo.txt", artigosRedeSocial);
 
-	}
+    }
 
 }
