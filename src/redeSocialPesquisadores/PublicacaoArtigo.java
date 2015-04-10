fa9a733cd10f9ecc3764b41d3966fbb533bf0a2e
diegotmd <diegotmd@gmail.com>
2015-04-10 14:10:07 -0300

    Atualização Fonte

--- src/redeSocialPesquisadores/PublicacaoArtigo.java
+++ src/redeSocialPesquisadores/PublicacaoArtigo.java
@@ -1,12 +1,13 @@
 package redeSocialPesquisadores;
 
 public class PublicacaoArtigo {
-	private Artigo artigo;
-	private Integer ordem;
-	
-	public PublicacaoArtigo (Artigo a, Integer o) {
-		this.artigo = a;
-		this.ordem = o;
-	}
-	
+    
+    private Artigo artigo;
+    private Integer ordem;
+
+    public PublicacaoArtigo (Artigo a, Integer o) {
+            this.artigo = a;
+            this.ordem = o;
+    }
+    
 }
