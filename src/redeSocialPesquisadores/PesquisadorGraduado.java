e78709cab463dae90cc840b686b17a78c8be47ef
diegotmd <diegotmd@gmail.com>
2015-04-10 14:08:59 -0300

    Atualização Fonte

--- src/redeSocialPesquisadores/PesquisadorGraduado.java
+++ src/redeSocialPesquisadores/PesquisadorGraduado.java
@@ -2,10 +2,14 @@ package redeSocialPesquisadores;
 
 public class PesquisadorGraduado extends Pesquisador {
 
-	@Override
-	public Double calcularPopularidade() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public PesquisadorGraduado(Long id, Integer qteHorasIC, Integer qteHorasED){
+        super(id, qteHorasIC, qteHorasED);
+    }	
+    
+    @Override
+    public Double calcularPopularidade() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
