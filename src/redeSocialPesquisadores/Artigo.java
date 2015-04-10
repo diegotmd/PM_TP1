363af427f06b1d4bf6dcf7acd1ee1b955cb9aa62
diegotmd <diegotmd@gmail.com>
2015-04-10 14:06:30 -0300

    Atualização Fonte

--- src/redeSocialPesquisadores/Artigo.java
+++ src/redeSocialPesquisadores/Artigo.java
@@ -4,41 +4,71 @@ import java.util.LinkedList;
 
 public class Artigo {
 
-	private Long idArtigo;
-	private Veiculo veiculoPublicado;
-	private LinkedList<Artigo> citadoPorArtigos;
-	
-	public Long getIdArtigo() {
-		return idArtigo;
-	}
+    private Long idArtigo;
+    private Veiculo veiculoPublicado;
+    private LinkedList<Artigo> citadoPorArtigos;
 
-	public void setIdArtigo(Long idArtigo) {
-		this.idArtigo = idArtigo;
-	}
+    public Artigo(Long id, Veiculo veiculo){
+        this.idArtigo = id;
+        this.veiculoPublicado = veiculo;
+        citadoPorArtigos = new LinkedList<Artigo>();
+    }
+    
+    public Long getIdArtigo() {
+        return idArtigo;
+    }
 
-	public Veiculo getVeiculoPublicado() {
-		return veiculoPublicado;
-	}
+    public Veiculo getVeiculoPublicado() {
+        return veiculoPublicado;
+    }
 
-	public void setVeiculoPublicado(Veiculo veiculoPublicado) {
-		this.veiculoPublicado = veiculoPublicado;
-	}
-	
-	public Double calcularQualidade(){
-		return 0.0;
-	}
-	
-	public void adicionarCitacao(Artigo artigo){
-		return;
-	}
-	
-	public static LinkedList<Artigo> carregarArtigos(String caminhoArquivo){
-		LinkedList<Artigo> artigos = new LinkedList<Artigo>(); 
-		return artigos;
-	}
-	
-	public static void carregarCitacoes(String caminhoArquivo, LinkedList<Artigo> artigos){
-		return;
-	}
+    public Double calcularQualidade(){
+        return 0.0;
+    }
+
+    /**
+    * Adiciona um registro à lista de artigos que citaram esta instância de artigo.
+    * 
+    * @param artigo Artigo citador.
+    */
+    public void adicionarCitacao(Artigo artigo){
+        citadoPorArtigos.add(artigo);
+        return;
+    }
+
+    /**
+    * Método estático. Lê o conteúdo de um arquivo contendo os artigos no formato
+    * "id_artigo;id_veiculo_publicador"
+    * 
+    * @param artigo Artigo citador.
+    * @param veiculos Lista de veículos existentes na rede social.
+    */
+    public static LinkedList<Artigo> carregarArtigos(String caminhoArquivo, LinkedList<Veiculo> veiculos){
+        LinkedList<Artigo> artigos = new LinkedList<Artigo>(); 
+        return artigos;
+    }
+
+    /**
+    * Método estático. Lê o conteúdo de um arquivo contendo as citações no formato
+    * "id_artigo_citado;id_artigo_citador", e carrega os dados na lista de artigos
+    * existentes na rede social, recebido como parâmetro.
+    * 
+    * @param artigo Artigo citador.
+    * @param veiculos Lista de veículos existentes na rede social.
+    */
+    public static void carregarCitacoes(String caminhoArquivo, LinkedList<Artigo> artigos){
+        return;
+    }
+    
+    /**
+    * Método estático. Escreve a qualidade dos artigos em um arquivo texto,
+    * um registro por linha, no formato "id_artigo;qualidade"
+    * 
+    * @param caminhoArquivo caminho completo do arquivo que será gerado
+    * @param artigos lista de veículos da rede social
+    */
+    public static void escreverQualidadeArtigos(String caminhoArquivo, LinkedList<Artigo> artigos) {
+        return;
+    }
 	
 }
