7fb3a8434f2c887ce2985f089801072ef78c9fe3
diegotmd <diegotmd@gmail.com>
2015-04-13 13:11:40 -0300

    Calculos e escrita de arquivos

--- src/redeSocialPesquisadores/PublicacaoArtigo.java
+++ src/redeSocialPesquisadores/PublicacaoArtigo.java
@@ -4,6 +4,14 @@ public class PublicacaoArtigo {
     
     private Artigo artigo;
     private Integer ordem;
+    
+    public Artigo getArtigo() {
+    	return this.artigo;
+    }
+    
+    public Integer getOrdem() {
+    	return this.ordem;
+    }
 
     public PublicacaoArtigo (Artigo a, Integer o) {
             this.artigo = a;
