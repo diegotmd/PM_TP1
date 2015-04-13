b489d1b06bcfc4d2a18c254eb1cb3b45ad3798a4
diegotmd <diegotmd@gmail.com>
2015-04-13 13:09:19 -0300

    commit diego

--- src/redeSocialPesquisadores/Artigo.java
+++ src/redeSocialPesquisadores/Artigo.java
@@ -1,7 +1,10 @@
 package redeSocialPesquisadores;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -26,7 +29,13 @@ public class Artigo {
     }
 
     public Double calcularQualidade(){
-        return 0.0;
+        Double fatorImpactoVeiculo = veiculoPublicado.calcularFatorImpacto();
+        Double qteCitacoes = (double) citadoPorArtigos.size();
+        return fatorImpactoVeiculo * qteCitacoes;
+    }
+    
+    public Integer quantidadeDeCitacoes() {
+        return citadoPorArtigos.size();
     }
 
     /**
@@ -58,13 +67,13 @@ public class Artigo {
         try {
         	arqArtigosVeiculos = new Scanner(new FileReader(caminhoArquivo)).useDelimiter("\\;|\\n");
         } catch (FileNotFoundException ex) {
-        	// tratar excecao de arquivo não encontrado
+        	// tratar excecao de arquivo nao encontrado
         }
         
         while (arqArtigosVeiculos.hasNextLong()) {
         	idArtigo = arqArtigosVeiculos.nextLong();
 
-        	// seleciona último campo como string e retira último caractere antes de converter para inteiro (quebra de linha)
+        	// seleciona ultimo campo como string e retira ultimo caractere antes de converter para Integereiro (quebra de linha)
         	strAux = arqArtigosVeiculos.next();
         	idVeiculo = Long.parseLong(strAux.substring(0, strAux.length() - 1));
         	
@@ -90,6 +99,43 @@ public class Artigo {
     * @param veiculos Lista de veiculos existentes na rede social.
     */
     public static void carregarCitacoes(String caminhoArquivo, LinkedList<Artigo> artigos){
+        // FALTA TESTAR
+        
+        Long idArtigo, idArtigoCitador;
+    	String strAux;
+        
+        Artigo artigo = null;
+        Artigo artigoCitador = null;
+        Scanner arqCitacoesArtigos = null;
+
+        try {
+            arqCitacoesArtigos = new Scanner(new FileReader(caminhoArquivo)).useDelimiter("\\;|\\n");
+        } catch (FileNotFoundException ex) {
+            // tratar excecao de arquivo nao encontrado
+        }
+        
+        while (arqCitacoesArtigos.hasNextLong()) {
+            idArtigo = arqCitacoesArtigos.nextLong();
+
+            // seleciona ultimo campo como string e retira ultimo caractere antes de converter para Integereiro (quebra de linha)
+            strAux = arqCitacoesArtigos.next();
+            idArtigoCitador = Long.parseLong(strAux.substring(0, strAux.length() - 1));
+
+            for (Artigo a : artigos) {
+                if (a.getIdArtigo().equals(idArtigo)) {
+                    artigo = a;
+                }
+                else if (a.getIdArtigo().equals(idArtigoCitador)) {
+                    artigoCitador = a;
+                }
+            }
+            
+            // adiciona artigo citador na lista
+            artigo.adicionarCitacao(artigoCitador);
+
+        }
+        
+        arqCitacoesArtigos.close();
         return;
     }
     
@@ -101,6 +147,20 @@ public class Artigo {
     * @param artigos lista de veiculos da rede social
     */
     public static void escreverQualidadeArtigos(String caminhoArquivo, LinkedList<Artigo> artigos) {
+        BufferedWriter output = null;
+        try {
+            output = new BufferedWriter(new FileWriter(caminhoArquivo, true));        
+            for (Artigo a : artigos) {
+                output.append(a.getIdArtigo()+ ";" + a.calcularQualidade());
+                output.newLine();
+            }
+
+            output.flush();
+            output.close();
+        } catch (IOException ex) {
+        	// tratar excecao de arquivo nao encontrado
+        }
+        
         return;
     }
 	
