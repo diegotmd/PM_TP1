cb2a0acd8e462439aac0c2f11a9f2c38a2561c0f
diegotmd <diegotmd@gmail.com>
2015-04-13 13:10:54 -0300

    Calculos e escrita de arquivos

--- src/redeSocialPesquisadores/PesquisadorDoutor.java
+++ src/redeSocialPesquisadores/PesquisadorDoutor.java
@@ -2,34 +2,38 @@ package redeSocialPesquisadores;
 
 public class PesquisadorDoutor extends Pesquisador {
 
-	private Integer qteAlunosGraduacaoOrientados;
-	private Integer qteAlunosMestradoOrientados;
-	private Integer qteAlunosDouturadoOrientados;
+    private Integer qteAlunosGraduacaoOrientados;
+    private Integer qteAlunosMestradoOrientados;
+    private Integer qteAlunosDouturadoOrientados;
 
-	public Integer getQteAlunosGraduacaoOrientados() {
-            return qteAlunosGraduacaoOrientados;
-	}
+    public Integer getQteAlunosGraduacaoOrientados() {
+        return qteAlunosGraduacaoOrientados;
+    }
 
-	public Integer getQteAlunosMestradoOrientados() {
+    public Integer getQteAlunosMestradoOrientados() {
         return qteAlunosMestradoOrientados;
-	}
+    }
 
-	public Integer getQteAlunosDouturadoOrientados() {
-            return qteAlunosDouturadoOrientados;
-	}
-        
-        public PesquisadorDoutor(Long id, Integer qteHorasIC, Integer qteHorasED, Integer qteAlunosGraduacao, Integer qteAlunosMestrado, Integer qteAlunosDoutorado){
-            super(id, qteHorasIC, qteHorasED);
-            this.qteAlunosGraduacaoOrientados = qteAlunosGraduacao;
-            this.qteAlunosMestradoOrientados = qteAlunosMestrado;
-            this.qteAlunosDouturadoOrientados = qteAlunosDoutorado;
-        }
+    public Integer getQteAlunosDouturadoOrientados() {
+        return qteAlunosDouturadoOrientados;
+    }
 
-	@Override
-	public Double calcularPopularidade() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public PesquisadorDoutor(Long id, Integer qteHorasIC, Integer qteHorasED, Integer qteAlunosGraduacao, Integer qteAlunosMestrado, Integer qteAlunosDoutorado){
+        super(id, qteHorasIC, qteHorasED);
+        this.qteAlunosGraduacaoOrientados = qteAlunosGraduacao;
+        this.qteAlunosMestradoOrientados = qteAlunosMestrado;
+        this.qteAlunosDouturadoOrientados = qteAlunosDoutorado;
+    }
 
+    @Override
+    public Double calcularPopularidade() {
+        // TODO Auto-generated method stub
+        return this.calcularPopularidadeBase() +
+               this.getQteHorasIniciacaoCientifica() +
+               this.getQteHorasEstagioDocencia() +
+               (10 * this.getQteAlunosGraduacaoOrientados()) +
+               (20 * this.getQteAlunosMestradoOrientados()) +
+               (30 * this.getQteAlunosDouturadoOrientados());
+    }
 	
 }
