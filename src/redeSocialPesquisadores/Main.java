38efaa0b816a8cfb3746581e3763e165abfedea7
diegotmd <diegotmd@gmail.com>
2015-04-13 21:12:42 -0300

    Formatação e tratamento de exceções

--- src/redeSocialPesquisadores/Main.java
+++ src/redeSocialPesquisadores/Main.java
@@ -1,5 +1,6 @@
 package redeSocialPesquisadores;
 
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class Main {
@@ -10,29 +11,38 @@ public class Main {
         LinkedList<Veiculo> veiculosRedeSocial;
         LinkedList<Pesquisador> pesquisadoresRedeSocial;
         
-        // le pesquisadores do arquivo
-        pesquisadoresRedeSocial = Pesquisador.carregarPesquisadores("pesquisadores.txt");
-        
-        // le veiculos do arquivo
-        veiculosRedeSocial = Veiculo.carregarVeiculos("veiculos.txt");
-        
-        // le artigos do arquivo
-        artigosRedeSocial = Artigo.carregarArtigos("artigos_veiculos.txt", veiculosRedeSocial);
-        
-        // le publicacoes de artigos pelos pesquisadores
-        Pesquisador.carregarPublicacoes("grafo_artigos_pesquisadores.txt", pesquisadoresRedeSocial, artigosRedeSocial);
-        
-        // le citacoes entre os artigos
-        Artigo.carregarCitacoes("grafo_citacoes.txt", artigosRedeSocial);
-        
-        // escreve popularidade dos pesquisadores
-        Pesquisador.escreverPopularidades("popularidade_pesquisador.txt", pesquisadoresRedeSocial);
-        
-        // escreve fator de impacto dos veiculos
-        Veiculo.escreverFatorImpactoVeiculos("fatorImpacto_veiculo.txt", veiculosRedeSocial);
-        
-        // escreve qualidade dos artigos
-        Artigo.escreverQualidadeArtigos("pontuacao_artigo.txt", artigosRedeSocial);
+        try {
+            // le pesquisadores do arquivo
+            pesquisadoresRedeSocial = Pesquisador.carregarPesquisadores("entrada15\\pesquisadores.txt");
+            
+            // le veiculos do arquivo
+            veiculosRedeSocial = Veiculo.carregarVeiculos("entrada15\\veiculos.txt");
+            
+            // le artigos do arquivo
+            artigosRedeSocial = Artigo.carregarArtigos("entrada15\\artigos_veiculos.txt", veiculosRedeSocial);
+            
+            // le publicacoes de artigos pelos pesquisadores
+            Pesquisador.carregarPublicacoes("entrada15\\grafo_artigos_pesquisadores.txt", pesquisadoresRedeSocial, artigosRedeSocial);
+            
+            // le citacoes entre os artigos
+            Artigo.carregarCitacoes("entrada15\\grafo_citacoes.txt", artigosRedeSocial);
+            
+            // escreve popularidade dos pesquisadores
+            Pesquisador.escreverPopularidades("entrada15\\popularidade_pesquisador.txt", pesquisadoresRedeSocial);
+            
+            // escreve fator de impacto dos veiculos
+            Veiculo.escreverFatorImpactoVeiculos("entrada15\\fatorImpacto_veiculo.txt", veiculosRedeSocial);
+            
+            // escreve qualidade dos artigos
+            Artigo.escreverQualidadeArtigos("entrada15\\pontuacao_artigo.txt", artigosRedeSocial);
+        }
+        catch (IOException ex) {
+        	System.out.println("Erro de leitura/escrita de arquivo: \r\n" + ex.getMessage());
+        }
+        catch (Exception ex) {
+        	System.out.println("Erro na execução: \r\n" + ex.getMessage());
+        }
+
 
     }
 
