14acf9cb7c95f666a92aad89e9b034d7a471221c
diegotmd <diegotmd@gmail.com>
2015-04-15 09:20:11 -0300

    Comentários

--- src/redeSocialPesquisadores/Conferencia.java
+++ src/redeSocialPesquisadores/Conferencia.java
@@ -1,5 +1,8 @@
 package redeSocialPesquisadores;
 
+/**
+ * Classe Conferencia, representa um tipo de Veículo de publicação, possui a própria forma de cálculo do fato de impacto.
+ */
 public class Conferencia extends Veiculo {
 
     public Conferencia(Long id){
