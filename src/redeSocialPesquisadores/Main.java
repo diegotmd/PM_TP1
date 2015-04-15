ef134bf8f42520e0e666d3e01d5bdda2700e7a86
Natércia Aguilar Batista <natercia.aguilar@gmail.com>
2015-04-15 19:07:52 -0300

    Leitura dos argumentos da linha de comando

--- src/redeSocialPesquisadores/Main.java
+++ src/redeSocialPesquisadores/Main.java
@@ -13,28 +13,28 @@ public class Main {
         
         try {
             // le pesquisadores do arquivo
-            pesquisadoresRedeSocial = Pesquisador.carregarPesquisadores("entrada15\\pesquisadores.txt");
+            pesquisadoresRedeSocial = Pesquisador.carregarPesquisadores(args[0]);
             
             // le veiculos do arquivo
-            veiculosRedeSocial = Veiculo.carregarVeiculos("entrada15\\veiculos.txt");
+            veiculosRedeSocial = Veiculo.carregarVeiculos(args[1]);
             
             // le artigos do arquivo
-            artigosRedeSocial = Artigo.carregarArtigos("entrada15\\artigos_veiculos.txt", veiculosRedeSocial);
+            artigosRedeSocial = Artigo.carregarArtigos(args[2], veiculosRedeSocial);
             
             // le publicacoes de artigos pelos pesquisadores
-            Pesquisador.carregarPublicacoes("entrada15\\grafo_artigos_pesquisadores.txt", pesquisadoresRedeSocial, artigosRedeSocial);
+            Pesquisador.carregarPublicacoes(args[3], pesquisadoresRedeSocial, artigosRedeSocial);
             
             // le citacoes entre os artigos
-            Artigo.carregarCitacoes("entrada15\\grafo_citacoes.txt", artigosRedeSocial);
+            Artigo.carregarCitacoes(args[4], artigosRedeSocial);
             
             // escreve popularidade dos pesquisadores
-            Pesquisador.escreverPopularidades("entrada15\\popularidade_pesquisador.txt", pesquisadoresRedeSocial);
+            Pesquisador.escreverPopularidades(args[5], pesquisadoresRedeSocial);
             
             // escreve fator de impacto dos veiculos
-            Veiculo.escreverFatorImpactoVeiculos("entrada15\\fatorImpacto_veiculo.txt", veiculosRedeSocial);
+            Veiculo.escreverFatorImpactoVeiculos(args[6], veiculosRedeSocial);
             
             // escreve qualidade dos artigos
-            Artigo.escreverQualidadeArtigos("entrada15\\pontuacao_artigo.txt", artigosRedeSocial);
+            Artigo.escreverQualidadeArtigos(args[7], artigosRedeSocial);
         }
         catch (IOException ex) {
         	System.out.println("Erro de leitura/escrita de arquivo: \r\n" + ex.getMessage());
