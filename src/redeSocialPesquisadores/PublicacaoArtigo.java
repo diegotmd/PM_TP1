42ba119b1d121600e4d2a15d59349c352768bef5
diegotmd <diegotmd@gmail.com>
2015-04-15 09:22:14 -0300

    Comentários

--- src/redeSocialPesquisadores/PublicacaoArtigo.java
+++ src/redeSocialPesquisadores/PublicacaoArtigo.java
@@ -1,5 +1,10 @@
 package redeSocialPesquisadores;
 
+/**
+ * Classe PublicacaoArtigo, representa a publicação de um artigo por um pesquisador em uma determinada ordem de participação.
+ * A decisão de utilizar esta classe foi para representar em uma só entidade a publicação de um artigo e a ordem que
+ * o pesquisador participou da publicação do mesmo.
+ */
 public class PublicacaoArtigo {
     
     private Artigo artigo;
