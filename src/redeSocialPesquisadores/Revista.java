85ec4d5a5b9b2f18231f64445b1845bfbaa820de
diegotmd <diegotmd@gmail.com>
2015-04-15 09:22:40 -0300

    Comentários

--- src/redeSocialPesquisadores/Revista.java
+++ src/redeSocialPesquisadores/Revista.java
@@ -1,5 +1,8 @@
 package redeSocialPesquisadores;
 
+/**
+ * Classe Revista, representa um tipo de Veículo de publicação, possui a própria forma de cálculo do fato de impacto.
+ */
 public class Revista extends Veiculo {
 
     public Revista(Long id){
